723e9243e5ec3895bbae1bb9e6dab1b9e33b0ce2
PLEES-Index <plees.index@gmail.com>
2023-03-24 22:28:23 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ import View from '/node_modules/ol/View.js';
 import {Fill, Stroke, Style} from '/node_modules/ol/style.js';
 
 //import dataset from './data/data.json' assert { type: 'JSON' };
-const dataset = fetch('/data/data.json');
+fetch('/data/data.json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    const dataset = data;
+  })
 
 // Set constants that are used throughout the code
 var scenario = "00_2010";
